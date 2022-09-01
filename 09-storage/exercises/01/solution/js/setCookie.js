5d917fa4696f5a1eaacac531951fb9573edb6be7
tamaslud <tamaslud@yahoo.com>
2022-09-01 17:28:56 +0200

    ???

--- 09-storage/exercises/01/solution/js/setCookie.js
+++ 09-storage/exercises/01/solution/js/setCookie.js
@@ -1,9 +1,10 @@
-const setCookie = (token = '') => {
+const setCookie = (myToken = '') => {
   const expired = new Date(Date.now() + 15 * 60 * 1000).toUTCString();
-  document.cookie = `token=${token}; expires=${expired};`;
+  document.cookie = `token=${myToken}; expires=${expired};`;
 };
 
 export default setCookie;
+
 /*
 1. Írj egy `setCookie` nevű függvényt, ami a paraméterként kapott stringet eltárolja
 egy `token` nevű sütiben, ami 15 perc után lejár!
